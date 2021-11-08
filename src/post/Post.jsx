4ada5b13518dc1d895f03f9ce5929a24bee2bf41
Kenan Kadic <ken.kadic@outlook.com>
2021-11-07 20:26:36 -0600

    post and posts pages updated with css

--- src/post/Post.jsx
+++ src/post/Post.jsx
@@ -5,19 +5,25 @@ export default function Post() {
         <div className="post">
             <img 
             className="postImg"
-            src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" 
+            src="https://images.pexels.com/photos/287227/pexels-photo-287227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" 
             alt="" 
             />
-        <div className="postinfo">
+        <div className="postInfo">
             <div className="postCats">
-                <span className="postCat">Fun</span>
-                <span className="postCat">Life</span>
+                <span className="postCat">Fun </span>
+                <span className="postCat">Life </span>
             </div>
             <span className="postTitle">Lorem ipsum dolor sit amet</span>
-            <hr/>
+            <hr />
             <span className="postDate">1 hour ago</span>
         </div>
+        <p className="postDesc">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Saepe, voluptatum soluta neque quam officia voluptatem ab nemo 
+            vitae quisquam et non excepturi animi cum cupiditate ipsa eligendi 
+            facere iste sed!
+        </p>
         </div>
             
-        )
+        );
 }

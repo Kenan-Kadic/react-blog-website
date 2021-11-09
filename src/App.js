1535d26b095aa7406b54488040b1fb37d034554f
Kenan Kadic <ken.kadic@outlook.com>
2021-11-09 14:26:42 -0600

    routes inserted in app js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import TopBar from "./components/topbar/TopBar";
+import Homepage from "./pages/homepage/Homepage";
 import Login from "./pages/login/Login";
-// import Settings from "./pages/settings/Settings";
-// import Homepage from "./pages/homepage/Homepage";
-// import Single from "./pages/single/Single";
-// import Write from "./pages/write/Write";
+import Register from "./pages/register/Register";
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
+    
     return (
-            <>
-                <TopBar/>
-                <Login/>
-            </>
+        <Router>
+            <TopBar/>
+            <Routes>
+                <Route exact path="homepage">
+                    <Homepage />
+                </Route>
+                <Route path="/posts">
+                    <Homepage />
+                </Route>
+                <Route path="/register">
+                    <Register />
+                </Route>
+                <Route path="/login">
+                    <Login />
+                </Route>
+                <Route path="/write">
+                    <Write />
+                </Route>
+                <Route path="/settings">
+                    <Settings />
+                </Route>
+                <Route path="/post/:postId">
+                    <Single />
+                </Route>
+            </Routes>        
+            </Router>
                    
     );
 }
